2ff0e0ef6d6bd25ca42c2521827384b64c967831
Nightrose2016 <lrbarrer@gmail.com>
2020-01-09 23:20:25 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 
   resources :worlds, only: [:create, :edit, :update, :index, :show, :new] do 
-    resources :characters, only: [:index, :show, :create, :new]
+    resources :characters, only: [:index, :show, :create, :new, :edit]
   end
   resources :characters, only: [:new, :create, :edit, :index, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
